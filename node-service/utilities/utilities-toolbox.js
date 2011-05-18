f811057849ada2da41f4f7e985bccfbfd172b524
Janne Julkunen <scorpio.iix@gmail.com>
2011-05-18 15:12:52 +0300

    Test release

--- node-service/utilities/utilities-toolbox.js
+++ node-service/utilities/utilities-toolbox.js
@@ -14,15 +14,10 @@ var utils = (function() {
 	that.extend = function(targetObject, sourceObject) {
 		for(key in sourceObject) {
 			if(typeof(sourceObject[key]) == 'object') {
-				if(sourceObject[key] instanceof Array) {
-					targetObject[key] = sourceObject[key];
-				}
-				else {
-					if(!targetObject[key])
-						targetObject[key] = {};
-			
-					that.extend(targetObject[key], sourceObject[key]);
-				}
+				if(!targetObject[key])
+					targetObject[key] = {};
+		
+				that.extend(targetObject[key], sourceObject[key]);
 			}
 			else
 				targetObject[key] = sourceObject[key];
