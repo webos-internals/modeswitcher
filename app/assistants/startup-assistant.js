87ddc05fe301d27b96ac16cf3db4f23b3705938f
Janne Julkunen <scorpio.iix@gmail.com>
2011-09-20 00:46:05 +0300

    New release

--- app/assistants/startup-assistant.js
+++ app/assistants/startup-assistant.js
@@ -15,7 +15,7 @@ function StartupAssistant() {
 
 	this.newMessages = [
 	{
-		version: '2.4.5', log: [
+		version: '2.4.6', log: [
 			'Triggers should now work better and be much more reliable than before.'
 		]
 	},
